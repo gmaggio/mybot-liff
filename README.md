acd99dcf299ab818c3a25cd220cc02068a852893
KieSP <eightyfour.sp@gmail.com>
2018-06-10 02:37:50 +0700

    implement and add readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-My First LIFF
+# My First LIFF
+
+Create a LIFF app with Reactjs
+
+### Installing
+
+Clone the repository
+
+```
+git clone https://github.com/kie-sp/myfirstliff.git
+```
+
+cd into myfirstliff directory and install
+
+```
+npm install
+```
+
+## Running 
+
+```
+npm start
+```
+
+
--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'muicss/react';
 
 const liff = window.liff;  
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayName : '',
+      userId : '',
+      pictureUrl : '',
+      statusMessage : ''
+    };
+    this.initialize = this.initialize.bind(this);
+    this.closeApp = this.closeApp.bind(this);
+  }
 
   componentDidMount() {
     window.addEventListener('load', this.initialize);
   }
 
   initialize() {
-    liff.init((data) => {});
+    liff.init(async (data) => {
+      let profile = await liff.getProfile();
+      this.setState({
+        displayName : profile.displayName,
+        userId : profile.userId,
+        pictureUrl : profile.pictureUrl,
+        statusMessage : profile.statusMessage
+      });
+    }); 
+  }
+
+  closeApp(event) {
+    event.preventDefault();
+    liff.sendMessages([{
+      type: 'text',
+      text: "Thank you, Bye!"
+    }]).then(() => {
+      liff.closeWindow();
+    });
   }
 
   render() {
@@ -21,9 +52,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <div style={{height:'20px'}}/>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Display Name : {this.state.displayName} <br/>
+          User ID : {this.state.userId} <br/>
+          Status Msg : {this.state.statusMessage}
         </p>
+        <Button color="primary" onClick={this.closeApp}>Close</Button>
       </div>
     );
   }
